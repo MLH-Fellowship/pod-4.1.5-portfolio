1379ede35d778109d7320f9b6e6c1b5d3223ad9d
Shikhar15606 <shikharshukla15606@gmail.com>
2021-11-11 02:58:03 +0530

    multiple fellows fix
    Signed-off-by: Shikhar15606 <shikharshukla15606@gmail.com>

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -1,16 +1,32 @@
 //let marker = L.marker([51.5, -0.09]).addTo(map);
 
-let imageOfPodMembers = ["daman"];
 
-let markerIcon = L.icon({
-  iconUrl: "/assets/img/daman.png",
-  iconSize: [50, 50], // size of the icon
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-  className: "markerImage",
-});
+let podMembersData = [
+  {
+    name: "Shikhar Shukla",
+    lat: 24.3956,
+    lng: 81.8825,
+    city: "Sidhi",
+    country: "India",
+    image: "shikhar.jpeg"
+  },{
+    name: "Sumit Shinde",
+    lat: 18.9754415,
+    lng: 73.0309219,
+    city: "Navi Mumbai",
+    country: "India",
+    image: "sumit.jpg"
+  },
+]
 
-imageOfPodMembers.map((name) => {
-  (markerIcon.iconUrl = `/assets/img/${name}.png`),
-    L.marker([45.4214, -75.6901], { icon: markerIcon }).addTo(map);
+podMembersData.map(memberData => {
+  let markerIcon = L.icon({
+    iconUrl: `/assets/img/map/${memberData.image}`,
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    className: "markerImage",
+  });
+  
+  L.marker([memberData.lat, memberData.lng], { icon: markerIcon }).addTo(map);
 });
