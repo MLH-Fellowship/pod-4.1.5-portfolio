36cd62a8ef6e1f0c2e6359a3f46024091793ca54
Shikhar15606 <shikharshukla15606@gmail.com>
2021-11-11 03:24:13 +0530

    added popup on the map icon

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,4 @@
-let map = L.map("map").setView([45.4214, -75.6901], 3);
+let map = L.map("map").setView([24.3956, 81.8825], 3);
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
