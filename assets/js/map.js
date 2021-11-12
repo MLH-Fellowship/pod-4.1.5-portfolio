a548288c9c727853aa81347275ec797681bb03fa
Shikhar Shukla <shikharshukla15606@gmail.com>
2021-11-12 22:24:35 +0530

    Making Everyone visible on initial render (#25)
    * Added everyone on map, few tweaks
    
    * Making Everyone visible on initial render

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,4 @@
-let map = L.map("map").setView([24.3956, 81.8825], 3);
+let map = L.map("map").setView([24.3956, 0], 2);
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
